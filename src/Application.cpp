1692a59d174086ed1b2bc23c5f99ca8a306f284d
Jason Burke <burkejasonj@icloud.com>
2022-02-21 02:40:33 -0800

    jsoncpp library read test. Formatted with clang-format

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,16 @@
 #include "include/Application.hpp"
 
-int main(int argc, char** argv)
-{
-  printf("Hello World!\n");
+int main(int argc, char **argv) {
+  Json::Value configRoot;
+  std::ifstream gameConfig("../data/default/canasta/BaseGameStorage.json",
+                           std::ifstream::binary);
+
+  gameConfig >> configRoot;
+
+  int ServerID = configRoot["Game"]["Settings"].get("ServerID", -1).asInt();
+
+  printf("ServerID: %i\n", ServerID);
+
+  gameConfig.close();
   return 0;
 }
--- src/include/Application.hpp
+++ src/include/Application.hpp
@@ -1,7 +1,8 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
-#include <stdio.h>
 #include "json/json.h"
+#include <fstream>
+#include <iostream>
 
 #endif
